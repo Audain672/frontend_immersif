e6f0328648faa9d44a9c3fd760b384570c1230ce
Audain672 <165726139+Audain672@users.noreply.github.com>
2025-06-04 08:59:16 +0100

    Update useChat.jsx fisrt

--- src/hooks/useChat.jsx
+++ src/hooks/useChat.jsx
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 
-const backendUrl = import.meta.env.VITE_API_URL || "https://backend-immersif.onrender.com";
+const backendUrl = import.meta.env.VITE_API_URL || "https://backend-immersif-1.onrender.com";
 console.log("URL du backend:", backendUrl);
 
 const ChatContext = createContext();
